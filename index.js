a2c51b5e8a08954a5e44ea6ff1a71a881da5e98d
Amal Mohammed <157481527+amolllat@users.noreply.github.com>
2024-02-22 23:07:44 +0300

    done

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a,b){
+    return a+b;
+}
+    console.log(add())
+
+function subtract(a,b) {
+    return a-b;
+}
+console.log(subtract())
+
+function multiply(a,b){
+    return a*b;
+}
+console.log(multiply())
+
+function divide(a, b) {
+    return a / b;
+  }
+  console.log(divide())
+
+  function increment(a) {
+    return a + 1 ;
+  }
+  console.log(increment())
+
+  function decrement(a) {
+    return a - 1 ;
+  }
+  console.log(decrement())
+
+  function makeInt(n) {
+    return parseInt(n,10);
+  }
+  console.log(makeInt())
+
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
